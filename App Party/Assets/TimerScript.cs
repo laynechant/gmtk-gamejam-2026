f9f900f889181e5407b7ccaf68b501cea36fce22
PotatoWZRD <jinglesd08@gmail.com>
2026-07-23 19:02:16 -0400

    simple hour and min timer in samplehometest scene

--- App Party/Assets/TimerScript.cs
+++ App Party/Assets/TimerScript.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 
 public class TimerScript : MonoBehaviour
 {
-
+    public int num;
     [SerializeField] TMP_Text Timer;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -14,6 +14,9 @@ public class TimerScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        int hours = Mathf.FloorToInt(num / 60F);
+        int min = Mathf.FloorToInt(num - hours * 60);
+
+        Timer.text = string.Format("{00}:{1:00}", hours, min);
     }
 }
